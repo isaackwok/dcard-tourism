10a89165a5aa73eb6969022368bfdd8761c8f1aa
Isaac <kch062522@gmail.com>
2021-02-10 05:28:51 +0800

    feat: bring logo and dropdowns to navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,13 +1,42 @@
 import Link from 'next/link';
 import allCities from '../data/citylist'
 import styles from '../styles/Navbar.module.css'
+import { useRouter } from 'next/router'
 
 export default function NavBar() {
     return (
-        <div className={styles.container}>
-            <Link href="/scenicSpot"><a className={styles.linkItem}>全部景點</a></Link>
-            { allCities.map(([cityCode, cityName]) =>
-                <Link href={`/scenicSpot/${cityCode}`} key={cityCode}><a className={styles.linkItem}>{cityName}</a></Link>) }
-        </div>
+        <nav className={styles.container}>
+            <Logo />
+            {/* { allCities.map(([cityCode, cityName]) =>
+                <Link href={`/scenicSpot/${cityCode}`} key={cityCode}><a className={styles.linkItem}>{cityName}</a></Link>) } */}
+            <CityDropdown />
+        </nav>
     )
 };
+
+function CityDropdown() {
+    const router = useRouter();
+    const handleDropdownChanged = (e) => {
+        const destinationRoute = e.target.value;
+        router.push(destinationRoute);
+    }
+    return (
+        <div className={styles.dropdownWrapper}>
+            <select className={styles.dropdown} onChange={handleDropdownChanged} value={`/scenicSpot/${router.query.city || ''}`}>
+                <option key='All' value='/scenicSpot/'>全部</option>
+                {allCities.map(([cityCode, cityName]) =>
+                    <option key={cityCode} value={`/scenicSpot/${cityCode}`}>{cityName}</option>)}
+            </select>
+        </div>
+    )
+}
+
+function Logo() {
+    return (
+        <Link href='/'>
+            <a>
+                <label className={styles.logo}>Tourism</label>
+            </a>
+        </Link>
+    )
+}
