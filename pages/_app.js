ae316dc01266e5d439d133f8285a84a4806e8153
Isaac <kch062522@gmail.com>
2021-02-10 05:29:13 +0800

    feat: split app into parts

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
 import '../styles/globals.css'
 import NavBar from '../components/NavBar'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Tourism</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <NavBar />
-      <Component {...pageProps} />
+      <div className='main'>
+        <Component {...pageProps} />
+      </div>
     </>
   )
 }
